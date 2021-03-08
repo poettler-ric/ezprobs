5bca56d4ca6c88fbb4d935c6770bc3771ab328ef
Richard Poettler <richard.poettler@gmail.com>
2021-03-08 16:41:39 +0100

    Added comments explaining every step

--- hydraulicstrainer/problems/xy.py
+++ hydraulicstrainer/problems/xy.py
@@ -21,6 +21,7 @@ bp = Blueprint("xy", __name__)
 
 @bp.route("/", methods=["POST", "GET"])
 def index():
+    # define initial state of the parameters
     parameter_a = Parameter(
         "a",
         "a",
@@ -32,15 +33,24 @@ def index():
     parameter_b = Parameter(
         "b", "b", -5, 5, val_initial=0, description="Offset of the function"
     )
+    # save them into the list passed later to the template
     parameters = [parameter_a, parameter_b]
+    # currently we don't have no calcluated solution, since we haven't been
+    # passed any parameters
     solution = None
 
+    # a POST request is used to pass parameters to the calculation
     if request.method == "POST":
+        # extract and cast the parameters
         a = int(request.form["a"])
         b = int(request.form["b"])
+        # set the initial values for the sliders
         parameter_a.val_initial = a
         parameter_b.val_initial = b
+        # pack the values needed when generating the solution section
         solution = {"a": a, "b": b}
+        # add the solution values to the session since the generation of the
+        # graph is a separate request
         session["solution"] = solution
 
     return render_template("problems/xy.html", parameters=parameters, solution=solution)
@@ -48,14 +58,18 @@ def index():
 
 @bp.route("/plot")
 def plot_function():
+    # extract the needd values from the session
     a = session["solution"]["a"]
     b = session["solution"]["b"]
 
+    # generate the plot
     fig, ax = plt.subplots()
     x = [0, 10]
     y = [i * a + b for i in x]
     ax.plot(x, y)
 
+    # save the plot to a buffer
     buffer = BytesIO()
     fig.savefig(buffer, format="png")
+    # stream the buffer to the client
     return Response(buffer.getvalue(), mimetype="image/png")
