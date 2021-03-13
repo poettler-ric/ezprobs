c16a52bce3faddee74da51ae8c055ee37640e228
Richard Poettler <richard.poettler@gmail.com>
2021-03-13 14:09:48 +0100

    Run calculation with initial parameters

--- hydraulicstrainer/problems/pressure_pipe_01.py
+++ hydraulicstrainer/problems/pressure_pipe_01.py
@@ -27,39 +27,9 @@ bp = Blueprint("pressure_pipe_01", __name__)
 
 @bp.route("/", methods=["POST", "GET"])
 def index():
-    parameter_d1 = Parameter(
-        "d1",
-        "d1",
-        5,
-        6,
-        val_initial=5.5,
-        val_step=0.1,
-        unit="cm",
-        description="Diameter of the pipe between I and II",
-    )
-    parameter_d2 = Parameter(
-        "d2",
-        "d2",
-        5,
-        6,
-        val_initial=5.5,
-        val_step=0.1,
-        unit="cm",
-        description="Diameter of the pipe between II and III",
-    )
-    parameter_d3 = Parameter(
-        "d3",
-        "d3",
-        5,
-        6,
-        val_initial=5.5,
-        val_step=0.1,
-        unit="cm",
-        description="Diameter of the pipe between III and VI",
-    )
-    parameters = [parameter_d1, parameter_d2, parameter_d3]
-    solution = None
-
+    d1 = 5.1
+    d2 = 5.1
+    d3 = 5.1
     ha = 260.0 * M
     hb = 197.2 * M
     h2 = 211.6 * M
@@ -77,64 +47,93 @@ def index():
         d1 = float(request.form["d1"])
         d2 = float(request.form["d2"])
         d3 = float(request.form["d3"])
-        parameter_d1.val_initial = d1
-        parameter_d2.val_initial = d2
-        parameter_d3.val_initial = d3
 
-        d1 *= CM
-        d2 *= CM
-        d3 *= CM
-        a1 = area_circle(d1 / 2)
-        a2 = area_circle(d2 / 2)
-        a3 = area_circle(d3 / 2)
+    parameter_d1 = Parameter(
+        "d1",
+        "d1",
+        5,
+        6,
+        val_initial=d1,
+        val_step=0.1,
+        unit="cm",
+        description="Diameter of the pipe between I and II",
+    )
+    parameter_d2 = Parameter(
+        "d2",
+        "d2",
+        5,
+        6,
+        val_initial=d2,
+        val_step=0.1,
+        unit="cm",
+        description="Diameter of the pipe between II and III",
+    )
+    parameter_d3 = Parameter(
+        "d3",
+        "d3",
+        5,
+        6,
+        val_initial=d3,
+        val_step=0.1,
+        unit="cm",
+        description="Diameter of the pipe between III and VI",
+    )
+    parameters = [parameter_d1, parameter_d2, parameter_d3]
 
-        q = fsolve(
-            lambda q: h4
-            - ha
-            + (q / a3) ** 2 / (2 / GRAVITY)
-            + local_loss(nu_entry, a1, q)
-            + pipe_loss(l1, a1, k, d1, q)
-            + pipe_loss(l2, a2, k, d2, q)
-            + pipe_loss(l3, a3, k, d3, q),
-            1,
-        )[0]
+    d1 *= CM
+    d2 *= CM
+    d3 *= CM
+    a1 = area_circle(d1 / 2)
+    a2 = area_circle(d2 / 2)
+    a3 = area_circle(d3 / 2)
 
-        v1 = q / a1
-        v2 = q / a2
-        v3 = q / a3
+    q = fsolve(
+        lambda q: h4
+        - ha
+        + (q / a3) ** 2 / (2 / GRAVITY)
+        + local_loss(nu_entry, a1, q)
+        + pipe_loss(l1, a1, k, d1, q)
+        + pipe_loss(l2, a2, k, d2, q)
+        + pipe_loss(l3, a3, k, d3, q),
+        1,
+    )[0]
 
-        x2 = sqrt(l1 ** 2 - (ha - h2) ** 2)
-        x3 = sqrt(l2 ** 2 - (h2 - h3) ** 2)
-        x4 = sqrt(l3 ** 2 - (h3 - h4) ** 2)
-        distances = np.array([0, x2, 0, x3, 0, x4])
+    v1 = q / a1
+    v2 = q / a2
+    v3 = q / a3
 
-        x = np.cumsum(distances)
-        pipe = np.array([ha, h2, h2, h3, h3, h4])
-        energy_horizon = np.full((len(x)), ha)
-        losses = np.array(
-            [
-                local_loss(nu_entry, a1, q),
-                pipe_loss(l1, a1, k, d1, q),
-                0,
-                pipe_loss(l2, a2, k, d2, q),
-                0,
-                pipe_loss(l3, a3, k, d3, q),
-            ]
-        )
-        cum_losses = np.cumsum(losses)
-        energy_line = energy_horizon - cum_losses
-        kinetic_energy = np.array([v1, v1, v2, v2, v3, v3]) ** 2 / (2 * GRAVITY)
-        pressure_line = energy_line - kinetic_energy
+    x2 = sqrt(l1 ** 2 - (ha - h2) ** 2)
+    x3 = sqrt(l2 ** 2 - (h2 - h3) ** 2)
+    x4 = sqrt(l3 ** 2 - (h3 - h4) ** 2)
+    distances = np.array([0, x2, 0, x3, 0, x4])
 
-        solution = {
-            "discharge": q,
-            "x": x.tolist(),
-            "pipe": pipe.tolist(),
-            "energy_horizon": energy_horizon.tolist(),
-            "energy_line": energy_line.tolist(),
-            "pressure_line": pressure_line.tolist(),
-        }
-        session["solution"] = solution
+    x = np.cumsum(distances)
+    pipe = np.array([ha, h2, h2, h3, h3, h4])
+    energy_horizon = np.full((len(x)), ha)
+    losses = np.array(
+        [
+            local_loss(nu_entry, a1, q),
+            pipe_loss(l1, a1, k, d1, q),
+            0,
+            pipe_loss(l2, a2, k, d2, q),
+            0,
+            pipe_loss(l3, a3, k, d3, q),
+        ]
+    )
+    cum_losses = np.cumsum(losses)
+    energy_line = energy_horizon - cum_losses
+    kinetic_energy = np.array([v1, v1, v2, v2, v3, v3]) ** 2 / (2 * GRAVITY)
+    pressure_line = energy_line - kinetic_energy
+
+    solution = {
+        "discharge": q,
+        "x": x.tolist(),
+        "pipe": pipe.tolist(),
+        "energy_horizon": energy_horizon.tolist(),
+        "energy_line": energy_line.tolist(),
+        "pressure_line": pressure_line.tolist(),
+    }
+    session["solution"] = solution
 
     return render_template(
         "problems/pressure_pipe_01.html", parameters=parameters, solution=solution
--- hydraulicstrainer/problems/xy.py
+++ hydraulicstrainer/problems/xy.py
@@ -21,37 +21,35 @@ bp = Blueprint("xy", __name__)
 
 @bp.route("/", methods=["POST", "GET"])
 def index():
-    # define initial state of the parameters
-    parameter_a = Parameter(
-        "a",
-        "a",
-        -5,
-        5,
-        val_initial=0,
-        description="Inclination of the function",
-    )
-    parameter_b = Parameter(
-        "b", "b", -5, 5, val_initial=0, description="Offset of the function"
-    )
-    # save them into the list passed later to the template
-    parameters = [parameter_a, parameter_b]
-    # currently we don't have no calcluated solution, since we haven't been
-    # passed any parameters
-    solution = None
+    a = 1
+    b = 1
 
     # a POST request is used to pass parameters to the calculation
     if request.method == "POST":
         # extract and cast the parameters
         a = int(request.form["a"])
         b = int(request.form["b"])
-        # set the initial values for the sliders
-        parameter_a.val_initial = a
-        parameter_b.val_initial = b
-        # pack the values needed when generating the solution section
-        solution = {"a": a, "b": b}
-        # add the solution values to the session since the generation of the
-        # graph is a separate request
-        session["solution"] = solution
+
+    # define initial state of the parameters
+    parameter_a = Parameter(
+        "a",
+        "a",
+        -5,
+        5,
+        val_initial=a,
+        description="Inclination of the function",
+    )
+    parameter_b = Parameter(
+        "b", "b", -5, 5, val_initial=b, description="Offset of the function"
+    )
+    # save them into the list passed later to the template
+    parameters = [parameter_a, parameter_b]
+
+    # pack the values needed when generating the solution section
+    solution = {"a": a, "b": b}
+    # add the solution values to the session since the generation of the
+    # graph is a separate request
+    session["solution"] = solution
 
     return render_template("problems/xy.html", parameters=parameters, solution=solution)
 
